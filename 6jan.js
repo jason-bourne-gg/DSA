57aec769acd35b8f5f6cc8ece6e88dcecd504567
Aniket Charjan (personal) <aniketcharjan3@gmail.com>
2024-01-06 18:52:40 +0530

    ez

---
+++ 6jan.js
@@ -0,0 +1,17 @@
+var subset = function (nums) {
+    let result = [[]];
+
+    function recurse (index, currArr) {
+        for (i=index; i<nums.length; i++){
+            
+
+        }
+    }
+    recurse (0,[])
+}
+
+// test case 1
+const nums = [1,2,3]
+console.log(subset(nums))
+
+// expeected output = [1], [1,2], [], .....[1,2,3]
