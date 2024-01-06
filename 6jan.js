bb196a4704a4ad86c98da09a221938e60d671b56
Aniket Charjan (personal) <aniketcharjan3@gmail.com>
2024-01-06 19:19:18 +0530

    other way

--- 6jan.js
+++ 6jan.js
@@ -17,6 +17,41 @@ var subset = function (nums) {
 
 // test case 1
 const nums = [1,2,3]
-console.log(subset(nums))
+// console.log(subset(nums))
 
 // expeected output = [1], [1,2], [], .....[1,2,3]
+
+
+// Alternate Solution
+
+  function getAllSubsets(arr) {
+  const result = [];
+
+  function generateSubsets(currentSubset, index) {
+    if (index === arr.length) {
+    // console.log([...currentSubset])
+      result.push([...currentSubset]);
+      return;
+    }
+
+    // Exclude the element at the current index
+    generateSubsets(currentSubset, index + 1);
+
+    // Include the element at the current index
+    currentSubset.push(arr[index]);
+    generateSubsets(currentSubset, index + 1);
+
+    // Backtrack to restore the original state for the next iteration
+    currentSubset.pop();
+  }
+
+  generateSubsets([], 0);
+
+  return result;
+}
+
+// Example usage:
+const inputArray = [1, 2, 3];
+const subsets = getAllSubsets(inputArray);
+
+console.log(subsets);
