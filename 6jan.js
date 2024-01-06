cf39ed49ac8142fd43877a77f418ad3cc3fecf74
Aniket Charjan (personal) <aniketcharjan3@gmail.com>
2024-01-06 19:01:26 +0530

    completed hard problem

--- 6jan.js
+++ 6jan.js
@@ -1,13 +1,18 @@
 var subset = function (nums) {
     let result = [[]];
 
+    // index, keeps tracks of nums number,currArr build subsets with that 
     function recurse (index, currArr) {
-        for (i=index; i<nums.length; i++){
-            
+        for (let i=index; i<nums.length; i++){
+            currArr.push(nums[i])
+            result.push([...currArr])
+            recurse(i+1,currArr)
+            currArr.pop()
 
         }
     }
     recurse (0,[])
+    return result
 }
 
 // test case 1
