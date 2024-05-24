f854e2a3ea747328fd007b52cd2aa49c8ccf31b7
jason-bourne-gg <car10@iitbbs.ac.in>
2024-05-24 10:58:25 +0530

    Solved avery good problem

---
+++ ARRAY/ZigZagPattern/solution.cpp
@@ -0,0 +1,26 @@
+/* Nice Problem */
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows <= 1) return s;
+        vector<string> ans(numRows, "");
+        int currRow = 0;
+        bool goDown = false;
+
+        for (auto i : s) {
+            // P
+            ans[currRow] += i;
+            // ["P", "", ""]
+            if (currRow == 0 || currRow == numRows - 1) goDown = !goDown;
+            // true
+            currRow += goDown ? 1 : -1;
+            // 1
+        }
+
+        string gg = "";
+        for (auto i : ans)
+            gg += i;
+        return gg;
+    }
+};
