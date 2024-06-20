5b04ad492d43d927241c381af50a2a6fa7abca5f
jason-bourne-gg <car10@iitbbs.ac.in>
2024-06-20 21:12:56 +0530

    another approach

--- TREE/rightView.cpp
+++ TREE/rightView.cpp
@@ -39,3 +39,52 @@ public:
         return ans;
     }
 };
+
+
+/* BFS using queue */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+
+        vector<int> res;
+        if(root== NULL) return {};
+        // function(root,0,res);
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int sz =q.size();
+             TreeNode*temp =NULL;
+            while(sz--)
+            {
+               temp= q.front();
+                q.pop();
+                if(temp->left ) q.push(temp->left);
+                if(temp->right ) q.push(temp->right);
+
+
+            }
+            res.push_back(temp->val);
+        }
+
+        return res;
+    }
+    // void function(TreeNode*root,int level,vector<int>& res){
+    //     if(root==NULL) return ;
+    //     if(res.size()==level) res.push_back(root->val);
+    //     function(root->right,level+1,res);
+    //     function(root->left,level+1,res);
+    // }
+};
