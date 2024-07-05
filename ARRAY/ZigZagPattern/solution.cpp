50468a3c3219cbc00971d83634ade0ae1d34be52
jason-bourne-gg <car10@iitbbs.ac.in>
2024-07-06 00:05:11 +0530

    added comment

--- ARRAY/ZigZagPattern/solution.cpp
+++ ARRAY/ZigZagPattern/solution.cpp
@@ -1,18 +1,24 @@
 /* Nice Problem */
 
-class Solution {
+// PAYPALISHIRING
+class Solution
+{
 public:
-    string convert(string s, int numRows) {
-        if(numRows <= 1) return s;
+    string convert(string s, int numRows)
+    {
+        if (numRows <= 1)
+            return s;
         vector<string> ans(numRows, "");
         int currRow = 0;
         bool goDown = false;
 
-        for (auto i : s) {
+        for (auto i : s)
+        {
             // P
             ans[currRow] += i;
             // ["P", "", ""]
-            if (currRow == 0 || currRow == numRows - 1) goDown = !goDown;
+            if (currRow == 0 || currRow == numRows - 1)
+                goDown = !goDown;
             // true
             currRow += goDown ? 1 : -1;
             // 1
