91c4415e5fceb286d628fe459f0d1171561a5bb4
jason-bourne-gg <car10@iitbbs.ac.in>
2024-07-21 22:37:30 +0530

    added logic

--- Graph/topoSort.cpp
+++ Graph/topoSort.cpp
@@ -3,17 +3,24 @@
 #include <stack>
 
 using namespace std;
-
+/* Intution behind using stack is..since the definition of
+topo sort is printing the nodes in such a way
+ that if there is an edge from u to v the  u should always come before v ;
+ Hence, we store nodes whose DFS is completed in stack and just print them
+ to get ONE of the possible solutions*/
 class Solution
 {
 private:
-    void dfs(int node, vector<bool> &vis, stack<int> &st, vector<int> &adj[])
+    void dfs(int node, vector<bool> &vis, stack<int> &st, vector<int> adj[])
     {
         vis[node] = true;
 
-        for (auto adj : adj[node])
+        for (auto neighbor : adj[node])
         {
-            dfs(adj, vis, st, adj);
+            if (!vis[neighbor])
+            {
+                dfs(neighbor, vis, st, adj);
+            }
         }
 
         st.push(node);
@@ -31,7 +38,9 @@ public:
         for (int i = 0; i < N; i++)
         {
             if (!vis[i])
+            {
                 dfs(i, vis, st, adj);
+            }
         }
 
         while (!st.empty())
@@ -43,3 +52,28 @@ public:
         return ans;
     }
 };
+
+int main()
+{
+    int N = 6;
+    vector<int> adj[6];
+
+    adj[5].push_back(2);
+    adj[5].push_back(0);
+    adj[4].push_back(0);
+    adj[4].push_back(1);
+    adj[2].push_back(3);
+    adj[3].push_back(1);
+
+    Solution sol;
+    vector<int> result = sol.getTopoSort(N, adj);
+
+    cout << "Topological Sort: ";
+    for (int i : result)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
