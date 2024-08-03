fdd1a6d081cb1e139565f287dc3bcdae5250fc14
jason-bourne-gg <car10@iitbbs.ac.in>
2024-08-03 15:17:08 +0530

    corected a mistake

--- Math/powx.cpp
+++ Math/powx.cpp
@@ -37,7 +37,7 @@ public:
         {
             x = 1 / x;
             n = INT_MAX;
-            return x * myPow(x, n - 1);
+            return x * myPow(x, n);
         }
 
         // If n is negative, compute the positive power and take the reciprocal
