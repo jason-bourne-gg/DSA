0b9a170d6af3b8ceed3325deab5d8642070b86f5
jason-bourne-gg <car10@iitbbs.ac.in>
2024-12-09 17:25:02 +0530

    LLD round of BrowserStack

---
+++ LLD/BrowserStack.js
@@ -0,0 +1,42 @@
+/* 
+function getBigChunk(fileName, size (bytes)) {
+    fs(file, size)
+    chunk = ""
+
+    chucnk = fs.method(size)
+
+    if (seek, byte) !=== " ",
+        {
+            while(seek !=== "" )
+    chunk + seek(),
+    }
+
+return chunk
+}
+
+
+function populateMap(threads, chunk, map) {
+    // distribute the chunk into smaller chunks and feed to thread for execution.
+}
+
+// main.js
+
+function main(fileName) {
+    const map = new hasMap();
+    const fileSize = fs.stat();
+    const size;
+
+    whille(size < fileSize){
+
+        const feedChunk = getBigChunk(fileName, 1024 * 1024 * 1024 * 1024)
+        size + = 1024 * 1024 * 1024 * 1024
+        populateMap(7, feedChunk, map);
+
+    }
+
+    display(map);
+
+}
+
+
+ */
